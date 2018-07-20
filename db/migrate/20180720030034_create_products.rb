a64fe21570037cc07b67cddacce034dc13c8107f
Adrian Dabi <adrian.dabi@hotmail.com>
2018-07-19 20:02:26 -0700

    4.1 fixes

--- db/migrate/20180720030034_create_products.rb
+++ db/migrate/20180720030034_create_products.rb
@@ -1,6 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.products :name
+      t.text :description
+      t.string :image_url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_190343) do
-
-  create_table "object_names", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
