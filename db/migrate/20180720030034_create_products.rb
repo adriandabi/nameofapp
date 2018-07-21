468dc0e09714139b46c02f325add16e251b7b71c
Adrian Dabi <adrian.dabi@hotmail.com>
2018-07-20 17:22:11 -0700

    4.1 delete 201 create object names

--- db/migrate/20180720030034_create_products.rb
+++ db/migrate/20180720030034_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.products :name
+      t.string :name
       t.text :description
       t.string :image_url
 
