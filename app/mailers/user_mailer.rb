fbfbdbd6b793620936becfbdf1d65bf0217206fe
Adrian Dabi <adrian.dabi@hotmail.com>
2018-08-15 16:05:10 -0700

    5.4 Authentication

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "adrian.dabi@hotmail.com"
 
   def contact_form(email, name, message)
     @message = message
-    mail(from: email,to: 'your-email@example.com',subject: "A new contact form message from #{name}")
+    mail(from: email,to: 'adrian.dabi@hotmail.com',subject: "A new contact form message from #{name}")
  end
 end
