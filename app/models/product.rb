503fc0cdc5d4435cf37abd6a6a2c9cb0a9d9de7c
Adrian Dabi <adrian.dabi@hotmail.com>
2018-08-30 02:19:43 -0700

    5.9 validation&Pagination

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,24 @@ class Product < ApplicationRecord
   def highest_rating_comment
     comments.rating_desc.first
   end
+  validates :name, presence: true
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
+  def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @product, notice: 'Review was created successfully.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
