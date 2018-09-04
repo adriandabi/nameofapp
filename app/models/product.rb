72a61c7858083693d08de7a42c03ee72b90e7588
Adrian Dabi <adrian.dabi@hotmail.com>
2018-09-04 16:07:42 -0700

    5.8 comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,5 +12,8 @@ class Product < ApplicationRecord
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
-  
+  def average_rating
+    comments.average(:rating).to_f
+  end
+
 end
