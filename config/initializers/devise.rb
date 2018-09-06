15e4f9efc5a2d7f33e7334a54115cd192bb17839
Adrian Dabi <adrian.dabi@hotmail.com>
2018-09-06 14:33:37 -0700

    5.9 devise initializing error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '8bf86f88b0a478ede2d12c378397cbb18ed5fe8a598adb9af45de7545ad112da74fbb5a2c55b8f2d09321456fc5ee72865da9d7232ff9a8a406e9fc299478cd8'
+   config.secret_key = '8bf86f88b0a478ede2d12c378397cbb18ed5fe8a598adb9af45de7545ad112da74fbb5a2c55b8f2d09321456fc5ee72865da9d7232ff9a8a406e9fc299478cd8'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
